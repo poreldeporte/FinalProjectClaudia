969c13cdce5435fb0dbd4b748273ebf480c786cd
Claudia Iturregui <claudia.iturregui@gmail.com>
2023-03-07 11:44:00 -0500

    "second"

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-const Under = require('../models/User')
+const User = require('../models/User')
 
 
 router.get('/profile/:userId', (req, res, next) => {
